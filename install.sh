3395156177d65755d1fa3fdeba67d7048d265f4a
Marcelo Machado Fleury <marcelomf@gmail.com>
2019-11-17 01:25:31 -0300

    Update dotfiles

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ ln -s $PWD".bashrc" ~/.bashrc
 ln -s $PWD".conkyrc" ~/.conkyrc
 ln -s $PWD".git" ~/.git
 ln -s $PWD".gitconfig" ~/.gitconfig
+ln -s $PWD".gitignore" ~/.gitignore
 ln -s $PWD".vimrc" ~/.vimrc
